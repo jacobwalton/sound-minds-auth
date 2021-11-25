615e9c8cf59c868f0ad4783caed1d709f936df7b
Jacob Walton <jawalton6616@gmail.com>
2021-11-25 06:55:04 -0600

    basic user model created

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-//connect to mongoose
+//Middleware
+app.use(cookieParser());
+app.use(bodyParser.json({ extended: true }));
+
+//Auth routes
+app.post("/api/register", (req, res) => {
+  const { username, email, password } = req.body;
+});
+
+//Connects to mongoose
 mongoose
   .connect(process.env.REACT_APP_MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("Successfully connected to DB!"))
---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: { type: String, unique: true, maxLength: 24 },
+    email: { type: String, unique: true, trim: true },
+    password: { type: String, minLength: 6 },
+  })
+);
