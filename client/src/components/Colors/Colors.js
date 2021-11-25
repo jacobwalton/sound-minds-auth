f774bc9a0f4ba74cdfec192d580a742f985ab179
Jacob Walton <jawalton6616@gmail.com>
2021-11-25 09:28:32 -0600

    added logos and css variables

---
+++ client/src/components/Colors/Colors.js
@@ -0,0 +1,15 @@
+import "./colors.css";
+
+const Colors = () => {
+  return (
+    <div className="colorPalete">
+      <div id="dark-blue" className="box"></div>
+      <div id="midnight" className="box"></div>
+      <div id="mint" className="box"></div>
+      <div id="orange" className="box"></div>
+      <div id="sky-blue" className="box"></div>
+    </div>
+  );
+};
+
+export default Colors;
