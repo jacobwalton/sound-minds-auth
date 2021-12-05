67add0c54adba6cfca8737e53c12dd61c8080d90
Jacob Walton <jawalton6616@gmail.com>
2021-12-05 08:11:01 -0600

    added profile page ui

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,7 +1,27 @@
 import "./profile.css";
+import { useContext } from "react";
+import UserContext from "../../helpers/UserContext";
+import Colors from "../../components/Colors/Colors";
 
 const Profile = () => {
-  return <div>PROFILE PAGE</div>;
+  const user = useContext(UserContext);
+  return (
+    <div className="profileContainer">
+      <div className="header">
+        <Colors />
+
+        <h1>{user.username}</h1>
+      </div>
+      <div className="userDetails">
+        <ul>
+          <li>Member since: </li>
+          <li>Number of comments: </li>
+          <li>Number of favorites: </li>
+        </ul>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
 };
 
 export default Profile;
