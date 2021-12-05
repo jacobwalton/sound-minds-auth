bad28955c4d4699d3f3fb2cbf5ff6b542a68a1f1
Jacob Walton <jawalton6616@gmail.com>
2021-12-05 08:26:47 -0600

    added search ui

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,7 +1,16 @@
 import "./search.css";
 
 const Search = () => {
-  return <div>SEARCH PAGE</div>;
+  return (
+    <div className="searchContainer">
+      <input
+        id="search"
+        type="text"
+        placeholder="Search for tracks, artists, or album..."
+        name="search"
+      />
+    </div>
+  );
 };
 
 export default Search;
