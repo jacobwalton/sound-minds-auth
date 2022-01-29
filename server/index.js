bd2ab081d0f4f5630b09ca161d2d9bdcd1a52bf7
Jacob Walton <jawalton6616@gmail.com>
2022-01-28 20:49:05 -0600

    serving from all routes

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
-app.use("/", express.static(path.join(__dirname, "/client/build")));
+app.use("*", express.static(path.join(__dirname, "/client/build")));
 
 // Gen route
 // if (process.env.NODE_ENV === "production") {
