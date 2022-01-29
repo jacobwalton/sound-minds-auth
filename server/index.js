31de0271089ebf7f5912cc5a38d0d0235413189b
Jacob Walton <jawalton6616@gmail.com>
2022-01-28 22:29:51 -0600

    cors issue appears to be solved. Still need to solve wildacrd issue to serve index.html from all routes

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
-app.get("/*", express.static(path.join(__dirname, "/client/build")));
+app.get("*", express.static(path.join(__dirname, "/client/build")));
 
 // app.all("*", function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
