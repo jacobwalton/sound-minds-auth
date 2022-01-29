b354ba2f925613a2334e50fb5b820bd3c619c579
Jacob Walton <jawalton6616@gmail.com>
2022-01-28 20:22:06 -0600

    removed /build from client gitignore

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.json({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
   });
 }
 
