33588c9f225d257ac7c56e890cad7ecdebe97007
Jacob Walton <jawalton6616@gmail.com>
2022-01-28 22:18:59 -0600

    fixing middleware & wildcard for route being served in server file again

--- server/index.js
+++ server/index.js
@@ -33,12 +33,11 @@ app.use(
 
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
-app.use("*", function (req, res, next) {
+app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
 
   express.static(path.join(__dirname, "/client/build"));
-  return next();
 });
 
 //Auth routes----
