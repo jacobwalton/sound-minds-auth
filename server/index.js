c2b049a03b3afc22a575e67535b3c3bfa5f4d4ae
Jacob Walton <jawalton6616@gmail.com>
2022-01-29 07:35:52 -0600

    debugging cors issue

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(
     cors({
       credentials: true,
-      "Access-Control-Allow-Origin": "https://sound-minds-jacob.herokuapp.com",
+      "Access-Control-Allow-Origin": true,
     })
   );
 }
