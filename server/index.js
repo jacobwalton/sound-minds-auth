adad4aa2f4d077253fc49c15b1c87ef5eb02b4b6
Jacob Walton <jawalton6616@gmail.com>
2022-01-28 20:10:57 -0600

    added general route to server file

--- server/index.js
+++ server/index.js
@@ -31,6 +31,14 @@ app.use(
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
 
+// Gen route
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Auth routes----
 //Log In
 app.get("/api/user", (req, res) => {
