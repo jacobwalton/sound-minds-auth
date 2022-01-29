616f7699f1eac64ba32ace0bc75184e758db5b35
Jacob Walton <jawalton6616@gmail.com>
2022-01-29 09:38:39 -0600

    debugging cors issues

--- server/index.js
+++ server/index.js
@@ -33,14 +33,14 @@ if (process.env.NODE_ENV === "production") {
     })
   );
 }
-let corsOptions = {
-  origin: "*",
-  credentials: true,
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Credentials": "true",
-};
+// let corsOptions = {
+//   origin: "*",
+//   credentials: true,
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Credentials": "true",
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
